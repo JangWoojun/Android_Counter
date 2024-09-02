dc44c5d0aa93018b78455453032fa45ff776f1e8
JangWoojun <woojun0107@naver.com>
2024-09-02 13:21:48 +0900

    feat: Counter 기능 완성

--- app/src/main/java/com/seogaemo/android_counter/MainActivity.kt
+++ app/src/main/java/com/seogaemo/android_counter/MainActivity.kt
@@ -1,20 +1,32 @@
 package com.seogaemo.android_counter
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private var number = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val numberTextView = findViewById<TextView>(R.id.number_text_view)
+        val resetButton = findViewById<Button>(R.id.reset_button)
+        val plusButton = findViewById<Button>(R.id.plus_button)
+
+        numberTextView.text = number.toString()
+
+        resetButton.setOnClickListener {
+            number = 0
+            numberTextView.text = number.toString()
+        }
+
+        plusButton.setOnClickListener {
+            number += 1
+            numberTextView.text = number.toString()
         }
     }
+
 }
